767dcdcb69abdac2caf2e05696fb01551cd0e352
NBarnes <nbarnes@gmail.com>
2013-05-08 13:38:06 -0700

    deadly_bot copied into fox_bot_v1

---
+++ bots/fox_bot.v1.rb
@@ -0,0 +1,17 @@
+class FoxBotV1 < RTanque::Bot::Brain
+  NAME = 'fox_bot_v1'
+  include RTanque::Bot::BrainHelper
+
+  def tick!
+    ## main logic goes here
+    
+    # use self.sensors to detect things
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
+    
+    # use self.command to control tank
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
+    
+    # self.arena contains the dimensions of the arena
+    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+  end
+end
